7075109afb36491e458348c7ed9acdd2da9ce8b9
isacostaf <iisabellecostaf@gmail.com>
2025-06-05 22:48:55 -0300

    4 tests

--- test/unit_test/off_product_dto_test.dart
+++ test/unit_test/off_product_dto_test.dart
@@ -5,8 +5,7 @@ import 'package:opennutritracker/features/add_meal/data/dto/off/off_product_nutr
 
 void main() {
   group('OFFProductDTO getLocaleName', () {
-    test('should return product_name_English when language is English', () {
-      // Arrange
+    test('Case 1: English Language - Valid Name', () {
       final product = OFFProductDTO(
         code: '123 - testValue',
         product_name: 'Name - testValue',
@@ -35,18 +34,15 @@ void main() {
         ),
       );
 
-      // Act
       final result = product.getLocaleName(SupportedLanguage.en);
 
-      // Assert
       expect(result, equals('English Name - testValue'));
     });
 
-    test('should return product_name_Deutscher when language is German', () {
-      // Arrange
+    test('Case 2: Deutscher Language - Valid Name', () {
       final product = OFFProductDTO(
         code: '123 - testValue',
-        product_name: 'Name - testValue',
+        product_name: 'Default Name - testValue',
         product_name_en: 'English Name - testValue',
         product_name_fr: 'Nom Français - testValue',
         product_name_de: 'Deutscher Name - testValue',
@@ -72,19 +68,16 @@ void main() {
         ),
       );
 
-      // Act
       final result = product.getLocaleName(SupportedLanguage.de);
 
-      // Assert
       expect(result, equals('Deutscher Name - testValue'));
     });
 
-    test('should return product_name_Deutscher when language is German', () {
-      // Arrange
+    test('Case 3: English Language - Null Name', () {
       final product = OFFProductDTO(
         code: '123 - testValue',
-        product_name: 'Name - testValue',
-        product_name_en: 'English Name - testValue',
+        product_name: 'Default Name - testValue',
+        product_name_en: null,
         product_name_fr: 'Nom Français - testValue',
         product_name_de: 'Deutscher Name - testValue',
         brands: 'Brand - testValue',
@@ -109,11 +102,55 @@ void main() {
         ),
       );
 
-      // Act
-      final result = product.getLocaleName(SupportedLanguage.de);
+      final result = product.getLocaleName(SupportedLanguage.en);
 
-      // Assert
-      expect(result, equals('Deutscher Name - testValue'));
+      expect(
+        result == 'Deutscher Name - testValue' ||
+        result == 'English Name - testValue' ||
+        result == 'Default Name - testValue' ||
+        result == 'Nom Français - testValue',
+        isTrue,
+      );
+    });
+
+    test('Case 4: English Language - Empty Name', () {
+      final product = OFFProductDTO(
+        code: '123 - testValue',
+        product_name: 'Default Name - testValue',
+        product_name_en: '',
+        product_name_fr: 'Nom Français - testValue',
+        product_name_de: 'Deutscher Name - testValue',
+        brands: 'Brand - testValue',
+        image_front_thumb_url: 'thumb - testValue',
+        image_front_url: 'front - testValue',
+        image_ingredients_url: 'ingredients - testValue',
+        image_nutrition_url: 'nutrition - testValue',
+        image_url: 'image - testValue',
+        url: 'url - testValue',
+        quantity: '100g - testValue',
+        product_quantity: '100 - testValue',
+        serving_quantity: '50 - testValue',
+        serving_size: '50g - testValue',
+        nutriments: OFFProductNutrimentsDTO(
+          energy_kcal_100g: 1,
+          carbohydrates_100g: 1,
+          fat_100g: 1,
+          proteins_100g: 1,
+          sugars_100g: 1,
+          saturated_fat_100g: 1,
+          fiber_100g: 1
+        ),
+      );
+
+      final result = product.getLocaleName(SupportedLanguage.en);
+
+      expect(
+        result == 'Deutscher Name - testValue' ||
+        result == 'English Name - testValue' ||
+        result == 'Default Name - testValue' ||
+        result == 'Nom Français - testValue',
+        isTrue,
+      );
     });
   });
 } 
