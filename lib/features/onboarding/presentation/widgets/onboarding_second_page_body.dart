0ff2a8d5439c45bc87b3bc9bb3ef488320121591
isacostaf <iisabellecostaf@gmail.com>
2025-07-01 11:06:47 -0300

    1 cycle: refactor

--- lib/features/onboarding/presentation/widgets/onboarding_second_page_body.dart
+++ lib/features/onboarding/presentation/widgets/onboarding_second_page_body.dart
@@ -183,14 +183,9 @@ class _OnboardingSecondPageBodyState extends State<OnboardingSecondPageBody> {
   String? validateWeight(String? value) {
     // if the user input is null, return the error message: Enter correct weight
     if (value == null) return S.of(context).onboardingWrongWeightLabel;
-
-    if(value == '0') {
-      return S.of(context).onboardingWrongWeightLabel;
-    }
-
     // if the user input is empty, return the error message: Enter correct weight
     // if the user input dont start with a number, return the error message: Enter correct weight
-    if (value.isEmpty || !RegExp(r'^[0-9]').hasMatch(value)) {
+    if (value.isEmpty || !RegExp(r'^[0-9]').hasMatch(value) || value == '0') {
       return S.of(context).onboardingWrongHeightLabel;
     } else {
       return null;
